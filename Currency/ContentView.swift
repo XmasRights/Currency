ffa25182551c66b9509ef6b4f82930a46b764eeb
Christopher Fonseka <chris.fonseka@googlemail.com>
2020-01-22 22:21:10 +0000

    Initial Commit

---
+++ Currency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Currency
+//
+//  Created by Christopher Fonseka on 22/01/2020.
+//  Copyright © 2020 Christopher Fonseka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
